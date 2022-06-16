77026af9857b6fb671f511500cc26155dcfb90ca
Todd Kushner <kushnertodd@gmail.com>
2022-06-16 12:44:54 -0600

    added kernel.cpp

---
+++ src/kernel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kushn on 6/16/2022.
+//
+
+#include "kernel.hpp"
---
+++ src/kernel.hpp
@@ -0,0 +1,16 @@
+//
+// Created by kushn on 6/16/2022.
+//
+
+#ifndef CV_WORKBENCH_SRC_KERNEL_HPP_
+#define CV_WORKBENCH_SRC_KERNEL_HPP_
+
+
+
+class kernel {
+
+};
+
+
+
+#endif //CV_WORKBENCH_SRC_KERNEL_HPP_
