0e02c0ebb706b514ef5e75b6c36ada8d1d00dac4
Todd Kushner <kushnertodd@gmail.com>
2022-06-26 17:58:42 -0600

    image-hough works but not sure the data is right...

--- src/image-hough.cpp
+++ src/image-hough.cpp
@@ -3,11 +3,16 @@
 #include <cstdlib>
 #include <fstream>      // std::ofstream
 #include <iostream>
+#include <vector>
 #include "variance_stats.hpp"
+#include "file_utils.hpp"
 #include "hough_theta.hpp"
+#include "wb_utils.hpp"
+#include "wb_defs.hpp"
 
 using namespace std;
 
+/*
 enum Cv_image_depth_enum {
   CV_8U,
   CV_8S,
@@ -19,20 +24,20 @@ enum Cv_image_depth_enum {
   CV_16F,
   UNDEFINED_IMAGE_DEPTH
 };
-
-string hist_filename;
-string hist_diff_filename;
+*/
 
 string depth_to_string(Cv_image_depth_enum depth) {
-  if (depth == CV_8U) return "CV_8U";
-  else if (depth == CV_32S) return "CV_32S";
-  else if (depth == CV_32F) return "CV_32F";
+  if (depth == CV_8U) return string("CV_8U");
+  else if (depth == CV_32S) return string("CV_32S");
+  else if (depth == CV_32F) return string("CV_32F");
   else return "invalid depth";
 }
 
+/*
 typedef unsigned char pixel_8U;
 typedef int pixel_32S;
 typedef float pixel_32F;
+*/
 Variance_stats variance_stats;
 
 void error_exit(string message) {
@@ -47,42 +52,175 @@ void read_int(FILE *fp, string name, int &var) {
   }
 }
 
+class Hough_class {
+ public:
+  string hough_filename;
+  static const int theta_inc = 3;
+  static const int nthetas = 180 / theta_inc;
+  static const int rho_buffer = 10;
+  static const int max_degree = 180;
+  static const int min_value = -5;
+  static const int max_value = 5;
+  int accum_val = 0;
+  int value_ct = 0;
+  int *accum[nthetas];
+  int max_rhos;
+  int rows;
+  int cols;
+
+  ~Hough_class() {
+    dealloc_accum();
+  }
+
+  Hough_class(int m_rows, int m_cols) :
+      rows(m_rows),
+      cols(m_cols) {
+    max_rhos = round(sqrt(rows * rows + cols * cols)) + rho_buffer;
+    int sq2 = round(sqrt(rows * rows + cols * cols));
+    alloc_accum();
+  }
+
+  int index_to_theta_deg(int index) {
+    return index * theta_inc;
+  }
+
+  float index_to_theta_cos(int index) {
+    return hough_cos[index];
+  }
+
+  float index_to_theta_sin(int index) {
+    return hough_sin[index];
+  }
+
+  void alloc_accum() {
+    for (int i = 0; i < nthetas; i++) {
+      accum[i] = new int[max_rhos];
+      for (int j = 0; j < max_rhos; j++)
+        accum[i][j] = 0;
+    }
+  }
+
+  int rho_to_index(int rho) {
+    return rho + max_rhos / 2;
+  }
+
+  int index_to_rho(int index) {
+    return index - max_rhos / 2;
+  }
+
+  int theta_rho_to_index(int theta, int rho) {
+    return theta * max_rhos + rho;
+  }
+
+  void assign_accum(int theta, int rho, int value) {
+    accum_val += abs(value);
+    value_ct++;
+    accum[theta][rho + max_rhos / 2] = value;
+  }
+
+//  void assign_accum(int index, int value) {
+//    accum[index / max_rhos][index % max_rhos] = value;
+//  }
+
+  void dealloc_accum() {
+    for (int i = 0; i < nthetas; i++)
+      delete accum[i];
+  }
+
+  int row_col_to_rho(int row, int col, int theta) {
+    int x = col - cols / 2;
+    int y = row - rows / 2;
+    return x * index_to_theta_cos(theta) + y * index_to_theta_sin(theta);
+  }
+
+  void write(string filename, string delim = "\t") {
+    ofstream ofs(filename, ofstream::out);
+    int max_val = 0;
+    if (!ofs)
+      throw "Hough:write invalid filename '" + filename + "'";
+    for (int rho = 0; rho < max_rhos; rho++)
+      ofs << rho << delim;
+    ofs << endl;
+    for (int theta = 0; theta < nthetas; theta++) {
+      ofs << index_to_theta_deg(theta) << delim;
+      for (int rho = 0; rho < max_rhos; rho++) {
+        ofs << accum[theta][rho] << delim;
+        int value = accum[theta][rho];
+        max_val = max(max_val,value);
+        if (value > 180)
+          cout << value << "\t" << theta << "\t" << rho << endl;
+      }
+      ofs << endl;
+    }
+    ofs.close();
+  }
+
+  void read(string filename) {
+    ifstream ifs(filename, ofstream::in);
+    if (!ifs)
+      throw "Hough:read invalid filename '" + filename + "'";
+    string line;
+    int index = 0;
+    while (getline(ifs, line)) {
+      vector<string> values = File_utils::string_split(line);
+      for (string value_str: values) {
+        int value;
+        if (!Workbench_utils::string_to_int(value_str, value))
+          throw "Hough::read: invalid value '" + value_str + "'";
+      }
+    }
+    ifs.close();
+  }
+
+};
+
 void stat_8U(pixel_8U *buf_8U, int rows, int cols) {
 }
 
+/*
 const int nthetas = 60;
 const int nrhos = 1000;
 const int hough_size = nthetas * nrhos;
+*/
 
-void stat_32S(pixel_32S *buf_32S, int rows, int cols) {
-  int max_rho = sqrt(rows * rows + cols * cols) + 10;
-  int min_value = -5;
-  int max_value = 5;
-
-  int *hough = new int[hough_size];
-  for (int rho = 0; rho < nrhos; rho++)
-    for (int theta = 0; theta < nthetas; theta++)
-      hough[rho * nthetas + theta] = 0;
-
+void stat_32S(pixel_32S *buf_32S, int rows, int cols, string filename) {
+  Hough_class hough(rows, cols);
+  int pos = 0;
+  int hist[2000];
+  int min_val = INT32_MAX;
+  int max_val = INT32_MIN;
+  for (int i = 0; i < rows * cols; i++) {
+    min_val = min(min_val, buf_32S[i]);
+    max_val = max(max_val, buf_32S[i]);
+  }
+  //for (int i = 0; i < rows*cols; i++) hist[buf_32S[i]-min_val]++;
   for (int row = 0; row < rows; row++) {
     for (int col = 0; col < cols; col++) {
-      for (int theta = 0; theta < nthetas; theta++) {
-        int val = buf_32S[row * cols + col];
-        if (val < min_value || val > max_value) {
-          int rho = round(hough_cos[theta] * col + hough_sin[theta] * row);
-          hough[rho * nthetas + theta]++;
+      //hist[buf_32S[pos]+1000]++;
+      //int value = buf_32S[pos++];
+      int index = row * cols + col;
+      if (col > 120) {
+        int a = 0;
+      }
+      int value = buf_32S[index];
+      static const int min_value = -5;
+      static const int max_value = 5;
+      hist[value - min_val]++;
+      //if (value < min_value || value > max_value) {
+      if (value != 0) {
+        for (int theta = 0; theta < Hough_class::nthetas; theta++) {
+          hough.assign_accum(theta, hough.row_col_to_rho(row, col, theta),
+                             abs(value));
         }
       }
     }
   }
-  ofstream ofs(hist_filename, ofstream::out);
-  for (int theta = 0; theta < nthetas; theta++) {
-    for (int rho = 0; rho < nrhos; rho++) {
-      ofs << hough[rho * nthetas + theta] << " ";
-    }
-    ofs << endl;
-  }
-  ofs.close();
+
+  ofstream o("hist.txt");
+  for (int i = 0; i < 2000; i++) o << i << " " << hist[i] << endl;
+  o.close();
+  string hough_filename = filename + ".hough.txt";
+  hough.write(hough_filename);
 }
 
 void stat_32F(pixel_32F *buf_32F, int rows, int cols) {
@@ -94,7 +232,6 @@ int main(int argc, char **argv) {
   if (argc < 2)
     error_exit("usage: image-stat filename");
   string filename = argv[1];
-  hist_filename = filename + ".hough.txt";
 
   FILE *fp = fopen(filename.c_str(), "r");
   if (fp == NULL) {
@@ -117,22 +254,27 @@ int main(int argc, char **argv) {
 
   pixel_32S *buf_32S;
   int newLen;
+  int hist[2000];
+  int min_val = INT32_MAX;
+  int max_val = INT32_MIN;
   switch (depth) {
     case CV_8U:
       break;
 
     case CV_32S:
       buf_32S = new pixel_32S[npixels];
+      for (int i = 0; i < rows * cols; i++) buf_32S[i] = 0;
       newLen = fread(buf_32S, sizeof(pixel_32S), npixels, fp);
-      //printf("ferror(fp) %d newLen %d sizeof(pixel_32S) %d npixels %d\n",
-      //       ferror(fp),
-      //       newLen,
-      //       sizeof(pixel_32S),
-      //       npixels);
       if (ferror(fp) != 0 || newLen != npixels) {
         error_exit("Image::read_binary: cannot read 32S image data in '" + filename + "'");
       }
-      stat_32S(buf_32S, rows, cols);
+
+      for (int i = 0; i < rows * cols; i++) {
+        min_val = min(min_val, buf_32S[i]);
+        max_val = max(max_val, buf_32S[i]);
+      }
+      for (int i = 0; i < rows * cols; i++) hist[buf_32S[i] - min_val]++;
+      stat_32S(buf_32S, rows, cols, filename);
       break;
 
     case CV_32F:
