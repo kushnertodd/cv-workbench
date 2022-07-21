67b83b5dee9375c01bcbd439dfc5c2cea5b1d4cd
Todd Kushner <kushnertodd@gmail.com>
2022-07-20 19:53:11 -0600

    interim sobel/hough test

--- src/test/filter-edge-sobel/test-filter-edge-sobel-clean.sh
+++ src/test/filter-edge-sobel/test-filter-edge-sobel-clean.sh
@@ -1,11 +1,9 @@
 image_dir=../../images
 for f in \
-  square45-90_sobel_gray_line_0_-162.jpg \
-  square45-90_sobel_gray_line_0_3.jpg \
-  square45-90_sobel_gray_line_15_-156.jpg \
-  square45-90_sobel_gray_line_15_-76.jpg \
-  square45-90_sobel_gray_line_45_295.jpg \
-  square45-90_sobel_gray_line_45_458.jpg 
+square45-90_gray_sobel_0_degrees.bin \
+square45-90_gray_sobel_0_degrees.jpg \
+square45-90_gray_sobel_90_degrees.bin \
+square45-90_gray_sobel_90_degrees.jpg
 do
   echo $image_dir/$f
   rm -f $image_dir/$f
