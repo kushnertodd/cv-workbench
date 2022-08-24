b56e6d161f3e157f27523a1482fc3e2ffa57a31d
Todd Kushner <kushnertodd@gmail.com>
2022-08-23 19:01:24 -0600

    one-pass-mean works for template matching

--- src/pearsons_correlation.cpp
+++ src/pearsons_correlation.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "pearsons_correlation.hpp"
+
+Pearsons_correlation::Pearsons_correlation(Image *m_image, Kernel *m_pattern) :
+    image(m_image),
+    pattern(m_pattern),
+    image_rows(image->get_rows()),
+    image_cols(image->get_cols()),
+    pattern_rows(pattern->get_rows()),
+    pattern_cols(pattern->get_cols()),
+    opm(new One_pass_mean(image, pattern_rows, pattern_cols)) {
+}
+
+Image *Pearsons_correlation::correlate() {
+  Image *output = new Image(*image);
+  for (int row = 0; row <= image_rows - pattern_rows; row++) {
+    for (int col = 0; col <= image_cols - pattern_cols; col++) {
+      output->set(row + opm->row_delta, col + opm->col_delta, opm->get_mean());
+      opm->col_right();
+    }
+    opm->row_down();
+  }
+  return output;
+}
--- src/pearsons_correlation.hpp
+++ src/pearsons_correlation.hpp
@@ -5,6 +5,10 @@
 #ifndef SRC__PEARSONS_CORRELATION_H_
 #define SRC__PEARSONS_CORRELATION_H_
 
+#include "image.hpp"
+#include "kernel.hpp"
+#include "one_pass_mean.hpp"
+
 /**
  * Computes a one-pass Pearson's correlation coefficient for matching a template against an image
  *
@@ -15,7 +19,16 @@
  *       sqrt([n * sum(x^2) - sum(x)^2] * [n * sum(y^2) - sum(y)^2])
  */
 class Pearsons_correlation {
-
+ public:
+  Image *image{};
+  Kernel *pattern{};
+  int image_rows{};
+  int image_cols{};
+  int pattern_rows{};
+  int pattern_cols{};
+  One_pass_mean *opm;
+  Pearsons_correlation(Image *m_image, Kernel *m_pattern);
+  Image *correlate();
 };
 
 #endif //SRC__PEARSONS_CORRELATION_H_
