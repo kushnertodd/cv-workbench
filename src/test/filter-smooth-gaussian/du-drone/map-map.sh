a474d28e2c29fdac3b0ff173f3feb3a019d293dc
Todd Kushner <kushnertodd@gmail.com>
2022-08-26 12:20:14 -0600

    cleaned up tests

--- src/test/filter-smooth-gaussian/du-drone/map-map.sh
+++
@@ -1,4 +0,0 @@
-for d in *drone*json ; do
-  echo $d
-  sed -e "s/drone/map/g" $d >../du-map/`echo $d | sed -e "s/drone/map/g"`
-done
--- src/test/filter-smooth-gaussian/du-map/run.sh
+++
@@ -1,7 +0,0 @@
-for f in `cat json.txt`; do
-  #echo $f
-  cd ../../..
-  echo ./cv-workbench test/filter-smooth-gaussian/du-map/$f
-  ./cv-workbench test/filter-smooth-gaussian/du-map/$f
-  cd test/filter-smooth-gaussian/du-map
-done
