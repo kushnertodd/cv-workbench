e5e656f7d08e9db1f6def373fc6c3fb229664230
Todd Kushner <kushnertodd@gmail.com>
2022-09-24 14:16:24 -0600

    interim checking linear edge filter

---
+++ src/operator_filter_edge_linear.cpp
@@ -0,0 +1,70 @@
+//
+// Created by kushn on 7/23/2022.
+//
+
+#include <memory>
+#include "kernel.hpp"
+#include "operator_utils.hpp"
+#include "operator_filter_smooth_gaussian.hpp"
+
+Operator_filter_edge_linear::~Operator_filter_edge_linear() = default;
+/**
+ * run image gaussian smooth operator
+ * @param input_data_sources
+ * @param output_data_stores
+ * @param operator_parameters
+ * @param log_entries
+ * @param errors
+ */
+void Operator_filter_edge_linear::run(std::list<Data_source_descriptor *> &input_data_sources,
+                                          std::list<Data_source_descriptor *> &output_data_stores,
+                                          String_map &operator_parameters,
+                                          std::list<WB_log_entry> &log_entries,
+                                          Errors &errors) {
+  if (input_data_sources.empty())
+    errors.add("Operator_filter_edge_linear::run", "", "input data source required");
+  if (input_data_sources.size() > 1)
+    errors.add("Operator_filter_edge_linear::run", "", "too many input data sources");
+  if (output_data_stores.empty())
+    errors.add("Operator_filter_edge_linear::run", "", "output data source required");
+  int rows;
+  Operator_utils::get_int_parameter("Operator_filter_edge_linear::run",
+                                    operator_parameters, "rows", rows, errors);
+  int cols;
+  Operator_utils::get_int_parameter("Operator_filter_edge_linear::run",
+                                    operator_parameters, "cols", cols, errors);
+  double sigma_x;
+  Operator_utils::get_real_parameter("Operator_filter_edge_linear::run",
+                                     operator_parameters, "sigma-x", sigma_x, errors);
+  double sigma_y;
+  Operator_utils::get_real_parameter("Operator_filter_edge_linear::run",
+                                     operator_parameters, "sigma-y", sigma_y, errors);
+  if (!errors.has_error()) {
+    Data_source_descriptor *input_data_source = input_data_sources.front();
+    std::unique_ptr<Image>
+        input(input_data_source->read_operator_image("Operator_filter_edge_linear::run", errors));
+    if (!errors.has_error())
+      input->check_grayscale("Operator_filter_edge_linear::run", errors);
+    if (!errors.has_error())
+      Operator_utils::get_subimage_parameters(input.get(),
+                                              "Operator_hough_image_create::run",
+                                              operator_parameters,
+                                              errors);
+    if (!errors.has_error()) {
+      std::unique_ptr<Kernel> gaussian_kernel_y(Kernel::create_gaussian_y(rows, sigma_y));
+      std::unique_ptr<Kernel> gaussian_kernel_x(Kernel::create_gaussian_x(cols, sigma_x));
+      std::unique_ptr<Image> output_pass1(gaussian_kernel_y->convolve_numeric(input.get(), errors));
+      if (!errors.has_error()) {
+        std::unique_ptr<Image> output2(gaussian_kernel_x->convolve_numeric(output_pass1.get(), errors));
+          for (Data_source_descriptor *output_data_store: output_data_stores)
+            if (!errors.has_error())
+            output_data_store->write_operator_image(output2.get(),
+                                                    "Operator_filter_edge_linear::run",
+                                                    errors);
+        if (!errors.has_error()) {
+          output2->log(log_entries);
+        }
+      }
+    }
+  }
+}
---
+++ src/operator_filter_edge_linear.hpp
@@ -0,0 +1,24 @@
+//
+// Created by kushn on 7/23/2022.
+//
+
+#ifndef SRC__OPERATOR_FILTER_EDGE_LINEAR_HPP_
+#define SRC__OPERATOR_FILTER_EDGE_LINEAR_HPP_
+
+#include "operator.hpp"
+
+/**
+ * Image gaussian smooth operator
+ */
+class Operator_filter_edge_linear : public Operator {
+ public:
+  ~Operator_filter_edge_linear() override;
+  void run(std::list<Data_source_descriptor *> &input_data_sources,
+           std::list<Data_source_descriptor *> &output_data_stores,
+           String_map &operator_parameters,
+           std::list<WB_log_entry> &log_entries,
+           Errors &errors) override;
+
+};
+
+#endif //SRC__OPERATOR_FILTER_EDGE_LINEAR_HPP_
