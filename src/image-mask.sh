977b4bcac39fc9902d4e4d2a776afe19e4358281
Todd Kushner <kushnertodd@gmail.com>
2022-09-27 18:27:07 -0600

    fixed config kernel rotate

--- src/image-mask.sh
+++ src/image-mask.sh
@@ -1,5 +1,6 @@
 #usage: image-mask out-filename rows cols theta-degrees height width-left value-left width-center value-center width-right value-right
 ./image-mask mask0.txt 30 30 0 20 3 -1 3 0 3 1 
 ./image-mask mask30.txt 30 30 30 20 3 -1 3 0 3 1 
+./image-mask mask45.txt 30 30 45 20 3 -1 3 0 3 1 
 ./image-mask mask60.txt 30 30 60 20 3 -1 3 0 3 1 
 ./image-mask mask90.txt 30 30 90 20 3 -1 3 0 3 1 
