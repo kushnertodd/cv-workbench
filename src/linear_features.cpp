98ec0ce4e62740976948fc39048f9885c6e2a28e
Todd Kushner <kushnertodd@gmail.com>
2022-10-03 19:27:56 -0600

    interim checking linear-features

---
+++ src/linear_features.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kushn on 10/3/2022.
+//
+
+#include "linear_features.hpp"
---
+++ src/linear_features.hpp
@@ -0,0 +1,16 @@
+//
+// Created by kushn on 10/3/2022.
+//
+
+#ifndef SRC__LINEAR_FEATURES_HPP_
+#define SRC__LINEAR_FEATURES_HPP_
+
+
+
+class linear_features {
+
+};
+
+
+
+#endif //SRC__LINEAR_FEATURES_HPP_
