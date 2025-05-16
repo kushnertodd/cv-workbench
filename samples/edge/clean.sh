1656a5a73c679ad48b85233895781329a572cfe7
Todd Kushner <kushnertodd@gmail.com>
2025-05-16 10:31:45 -0600

    sobel cleanup

--- samples/edge/clean.sh
+++ samples/edge/clean.sh
@@ -1,7 +1,1 @@
-rm image_color_edge.bin
-rm image_color_edge.jpg
-rm image_color_edge.log
-rm image_grayscale_edge.bin
-rm image_grayscale_edge.jpg
-rm image_grayscale_edge_threshold.jpg
-rm image_grayscale_edge.log
+rm -f `cat clean.txt`
